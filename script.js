d9de7aad8939eb3a87122677da07f6b84655c56f
Kevin Jamieson <kevinjamiesondev@gmail.com>
2024-08-21 13:09:21 -0500

    Created choice for human

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 console.log("Hello World");
 
+console.log("Hello World!");
+
+function getComputerChoice() {
+    let choice = ["rock", "paper", "scissors"]; // Choices for computer to play
+    let getComputerChoice = (Math.random(Math.floor) * choice.length);
+    return choice[randomIndex];
+}
+
+console.log(getComputerChoice);
+
+function getHumanChoice() {
+    let userInput = prompt("Enter your choice (rock, paper, or scissors):");
+
+    userInput = userInput.toLowerCase();
+
+    if (userInput === "rock" || userInput === "paper" || "scissors") {
+        return userInput;
+    } else {
+        return "Invaid choice. Please enter rock, paper, or scissors.";
+    }
+}
+
+console.log(getHumanChoice());
